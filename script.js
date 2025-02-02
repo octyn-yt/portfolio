2247e9189988b4c4259748591e6c8873ef8b38d1
Jes <141727697+octyn-yt@users.noreply.github.com>
2025-02-02 02:51:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-// JavaScript for Dark Mode Toggle
+// Check for dark mode in localStorage and apply it
+if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+}
+
+// Dark Mode Toggle
 const themeToggleButton = document.getElementById('theme-toggle');
 themeToggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+
+    // Save the user's dark mode preference
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+    }
 });
 
-// JavaScript for Burger Menu Toggle
+// Burger Menu Toggle
 document.getElementById('burger-menu').addEventListener('click', function() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('open');
