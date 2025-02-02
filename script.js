88c020f6a22a3355283680aee10c0d9fb1312eaa
Jes <141727697+octyn-yt@users.noreply.github.com>
2025-02-02 01:56:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,17 @@ const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme === 'dark') {
     document.body.classList.add('dark-mode');
-    themeToggleButton.classList.add('dark-mode');
-    themeToggleButton.innerHTML = '<i class="fas fa-sun"></i>';  // Change to Sun icon
+    themeToggleButton.innerHTML = '<i class="fas fa-sun"></i>';  // Change to Sun icon for dark mode
 } else {
     document.body.classList.remove('dark-mode');
-    themeToggleButton.classList.remove('dark-mode');
-    themeToggleButton.innerHTML = '<i class="fas fa-moon"></i>';  // Keep Moon icon
+    themeToggleButton.innerHTML = '<i class="fas fa-moon"></i>';  // Default to Moon icon for light mode
 }
 
 // Add event listener to toggle theme
 themeToggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     
-    // Toggle theme button icon
+    // Toggle button icon
     if (document.body.classList.contains('dark-mode')) {
         themeToggleButton.innerHTML = '<i class="fas fa-sun"></i>';
         localStorage.setItem('theme', 'dark');  // Save theme to localStorage
