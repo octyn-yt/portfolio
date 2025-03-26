db67d56a46861563d7eda4e10c68914304767bc9
Jes <141727697+octyn-yt@users.noreply.github.com>
2025-03-26 22:09:42 +0000

    removed more unnecessary comments

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Check for dark mode in localStorage and apply it
 if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
 }
 
-// Dark Mode Toggle
 const themeToggleButton = document.getElementById('theme-toggle');
 themeToggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 
-    // Save the user's dark mode preference
     if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('darkMode', 'enabled');
     } else {
@@ -16,13 +13,11 @@ themeToggleButton.addEventListener('click', () => {
     }
 });
 
-// Burger Menu Toggle
 document.getElementById('burger-menu').addEventListener('click', function() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('open');
 });
 
-// Clipboard copy function for donation grid items
 const paymentMethods = document.querySelectorAll('.payment-method');
 paymentMethods.forEach((method) => {
     method.addEventListener('click', () => {
